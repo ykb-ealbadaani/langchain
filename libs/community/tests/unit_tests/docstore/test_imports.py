ad2cda674a7876ebc4f470386cfdd61d736735e7
ykb-ealbadaani <109969913+ykb-ealbadaani@users.noreply.github.com>
2024-04-09 00:04:43 +0300

    Delete libs directory

--- libs/community/tests/unit_tests/docstore/test_imports.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.docstore import __all__
-
-EXPECTED_ALL = ["DocstoreFn", "InMemoryDocstore", "Wikipedia"]
-
-
-def test_all_imports() -> None:
-    assert set(__all__) == set(EXPECTED_ALL)
--- libs/langchain/tests/unit_tests/docstore/test_imports.py
+++
@@ -1,9 +0,0 @@
-from langchain import docstore
-from tests.unit_tests import assert_all_importable
-
-EXPECTED_ALL = ["DocstoreFn", "InMemoryDocstore", "Wikipedia"]
-
-
-def test_all_imports() -> None:
-    assert set(docstore.__all__) == set(EXPECTED_ALL)
-    assert_all_importable(docstore)
